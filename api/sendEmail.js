3a7f1766c435eacdb99ca0497151c6b9109ce54e
Alan B <alanbaltic@hotmail.com>
2025-10-31 17:55:11 +0000

    Fix Vercel API routing for sendEmail

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
   if (req.method !== "POST") {
-    res.status(405).send("Method not allowed");
-    return;
+    return res.status(405).send("Method not allowed");
   }
 
   res.status(200).json({ success: true, message: "API connected successfully!" });
